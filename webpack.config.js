92be65efb31f2224a8a83fde24a713e57593d93d
Stan Chan <stanchanpsu@outlook.com>
2017-07-07 13:51:00 -0400

    Rename client to src, add prod build script

--- webpack.config.js
+++ webpack.config.js
@@ -2,14 +2,14 @@ const path = require('path');
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
-  'template': './client/index.html',
+  'template': './src/index.html',
   'filename': 'index.html',
   'inject': 'body'
 });
 
 
 module.exports = {
-  'entry': './client/index.js',
+  'entry': './src/index.js',
   'output': {
     'path': path.resolve(__dirname, 'dist'),
     'filename': 'bundle.js'
